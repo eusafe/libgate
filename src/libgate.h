d3d2bb30f5af66e03a481da08f4e9a9a0eb25944
Evgeny <eu.safeschool@gmail.com>
2012-09-05 00:33:12 +0200

    current

--- src/libgate.h
+++ src/libgate.h
@@ -1,3 +1,12 @@
+/*
+ *
+ * Adapter for controller of gate
+ * 
+ * Designed by Evgeny Byrganov <eu dot safeschool at gmail dot com> for safeschool.ru, 2012
+ *  
+ * $Id$
+ *
+ */
 
 
 #ifndef _LIBGATE_H
